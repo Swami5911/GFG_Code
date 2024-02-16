8453af549eca40ff7016e089cdb814495f733070
Pardeep swami <105664755+Swami5911@users.noreply.github.com>
2024-02-16 22:30:55 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Remove all duplicates from a given string/remove-all-duplicates-from-a-given-string.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	string removeDuplicates(string str) {
+	    // code here
+	     string ans = "";
+        unordered_set<char> seen;
+
+        for (char c : str) {
+            if (seen.find(c) == seen.end()) {
+                ans.push_back(c);
+                seen.insert(c);
+            }
+        }
+
+        return ans;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string str;
+        cin >> str;
+        Solution ob;
+        auto ans = ob.removeDuplicates(str);
+
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
