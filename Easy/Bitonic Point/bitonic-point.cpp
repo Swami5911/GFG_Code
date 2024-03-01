d49339150246412d170dd685e1dc17d31ce33756
Pardeep swami <105664755+Swami5911@users.noreply.github.com>
2024-03-01 22:00:38 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Bitonic Point/bitonic-point.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	
+	int findMaximum(int arr[], int n) {
+	    // code here
+	    int i=0;
+        int j=n-1;
+        
+        while(i<=j){
+            if(arr[i] < arr[j]){
+                i++;
+            }else{
+                j--;
+            }
+        }
+        
+        return max(arr[i],arr[j]);
+
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.findMaximum(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
