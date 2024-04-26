30b1d4792063555370010e34ccfd546b2cf68ec9
Pardeep swami <105664755+Swami5911@users.noreply.github.com>
2024-04-26 12:07:26 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Maximum sum of hour glass/maximum-sum-of-hour-glass.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int findMaxSum(int n, int m, vector<vector<int>> mat) {
+         int max_sum=INT_MIN;
+        for(int i=0;i<n-2;i++){
+            for(int j=0;j<m-2;j++){
+                int hourglass_sum={mat[i][j]+mat[i][j+1]+mat[i][j+2]+mat[i+1][j+1]+mat[i+2][j]+mat[i+2][j+1]+mat[i+2][j+2]};
+            
+                max_sum=max(max_sum,hourglass_sum);
+            }
+        }
+        if(max_sum==INT_MIN){
+            return -1;
+        }
+        else{
+            return max_sum;
+        }
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, M, i, j;
+        cin >> N >> M;
+        vector<vector<int>> Mat(N, vector<int>(M));
+        for (i = 0; i < N; i++)
+            for (j = 0; j < M; j++) cin >> Mat[i][j];
+        Solution ob;
+        cout << ob.findMaxSum(N, M, Mat) << "\n";
+    }
+}
+// } Driver Code Ends
