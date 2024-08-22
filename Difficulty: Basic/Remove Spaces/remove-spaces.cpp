1bd553a4b9b03b7ea9d9a8f86f88dc54efa22c50
Pardeep swami <105664755+Swami5911@users.noreply.github.com>
2024-08-22 19:50:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remove Spaces/remove-spaces.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    string modify (string s)
+    {
+    string temp;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == ' ') continue;
+            temp += s[i];
+        }
+        return temp;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore ();
+    while (t--)
+    {
+        string s;
+        getline (cin, s);
+        Solution ob;
+        cout <<ob.modify (s) << endl;
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
